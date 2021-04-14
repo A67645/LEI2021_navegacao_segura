da99cf84c46529fdf76fd95622422dc958b5267a
Moisés Ramires <47677390+moisesramires@users.noreply.github.com>
2021-04-14 23:27:30 +0100

    Added Reddit Script, only scroll

---
+++ scripts/reddit.py
@@ -0,0 +1,34 @@
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium.webdriver.common.keys import Keys
+import time
+import random
+
+
+
+options = EdgeOptions()
+options.use_chromium = True
+
+driver = Edge(options=options)
+driver.get("https://www.reddit.com/")
+driver.maximize_window()
+time.sleep(2)
+driver.execute_script("window.scrollTo(0, 300)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(300, 600)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(600, 900)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(900, 1200)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(1200, 1500)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(1500, 1800)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(1800, 2100)") 
+time.sleep(4)
+driver.execute_script("window.scrollTo(2100, 2400)") 
+
+
+
+time.sleep(4)
+driver.close()
